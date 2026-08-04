951a2ad0eee24a70389f9bda9f317a2380c818c0
theCreatorJona <jonaaadrift@gmail.com>
2026-08-04 12:16:17 +0200

    update: Main.java

--- Main.java
+++ Main.java
@@ -9,9 +9,14 @@ public class Main{
         
         Scanner input = new Scanner(System.in);
         
-        Player player = new Player("Jonathan", 300.0f);
+        Player player = new Player("Jonathan");
+        
+        Portal PortalEIC = new Portal("EIC", 1, 0, 0, 100, 100, 1);
         
         while (!player.quitPlayer){
+            // logica global
+            player.playerIsQuit(player.actionPlayer);
+            
             
             if (player.roomPlayer.equals(Room.MENU)){
                 System.out.println("At the menu!");
@@ -23,8 +28,10 @@ public class Main{
             else if (player.roomPlayer.equals(Room.GAME)){
                 
                 player.informationPlayer();
-                player.movementPlayer(player.actionPlayer);
+                PortalEIC.portalIsActivate(player);
                 player.actionPlayer = input.nextLine();
+                player.movementPlayer(player.actionPlayer);
+                
                 player.changeModePlayer(player.actionPlayer);
             }
             
@@ -33,7 +40,6 @@ public class Main{
                 System.out.print("Chose your mode: ");
                 player.actionPlayer = input.nextLine();
             }
-            else { player.roomPlayer = Room.MENU; }
             
         }
         
